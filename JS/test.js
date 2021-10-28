95dd5f5a689cd79515f3b03b4e947ffd2b344e40
MadBabyBrain <46905566+MadBabyBrain@users.noreply.github.com>
2021-10-28 17:20:06 +0100

    test

--- JS/test.js
+++ JS/test.js
@@ -66,15 +66,15 @@ window.onload = () => {
 }
 
 window.onbeforeunload = () => {
-    localStorage.getItem('level', level);
-    localStorage.getItem('prestige_level', prestige_level);
-    localStorage.getItem('prestige_mult', prestige_mult);
-    localStorage.getItem('enemy', enemy.hp);
-    localStorage.getItem('player_click', player.click);
-    localStorage.getItem('player_dmg', player.damage);
-    localStorage.getItem('shop_money', shop.money);
-    localStorage.getItem('shop_dmg_cost', shop.DPScost);
-    localStorage.getItem('shop_click_cost', shop.CLICKcost);
+    // localStorage.getItem('level', level);
+    // localStorage.getItem('prestige_level', prestige_level);
+    // localStorage.getItem('prestige_mult', prestige_mult);
+    // localStorage.getItem('enemy', enemy.hp);
+    // localStorage.getItem('player_click', player.click);
+    // localStorage.getItem('player_dmg', player.damage);
+    // localStorage.getItem('shop_money', shop.money);
+    // localStorage.getItem('shop_dmg_cost', shop.DPScost);
+    // localStorage.getItem('shop_click_cost', shop.CLICKcost);
 }
 
 var increaseDPS = () => {
