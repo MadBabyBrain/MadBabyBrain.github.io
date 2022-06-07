116a956b03f6d18684f9ff662405c3e3fcec9ab6
MadBabyBrain <46905566+MadBabyBrain@users.noreply.github.com>
2022-06-07 01:09:15 +0100

    update

--- SoftBody/BUILD/controller.js
+++ SoftBody/BUILD/controller.js
@@ -1,6 +1,6 @@
 var drawer = null;
 var create = function () {
     var can = document.createElement('canvas');
-    document.getElementsByClassName("container").item(0).appendChild(can);
+    document.getElementById("container").appendChild(can);
     drawer = new Drawer(can);
 };
--- SoftBody/BUILD_TS/controller.ts
+++ SoftBody/BUILD_TS/controller.ts
@@ -2,6 +2,6 @@ let drawer: Drawer | null = null;
 
 let create = () => {
     let can = document.createElement('canvas') as HTMLCanvasElement;
-    document.getElementsByClassName("container").item(0).appendChild(can);
+    document.getElementById("container").appendChild(can);
     drawer = new Drawer(can);
 }
