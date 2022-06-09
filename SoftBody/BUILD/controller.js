105c1067c3467f1e4b2009d092fbf6bd4c08fc6b
MadBabyBrain <46905566+MadBabyBrain@users.noreply.github.com>
2022-06-09 23:06:02 +0100

    disallowed multiple canvas's

--- SoftBody/BUILD/controller.js
+++ SoftBody/BUILD/controller.js
@@ -1,9 +1,11 @@
 var drawer = null;
 var create = function () {
-    var can = document.createElement('canvas');
-    can.setAttribute('id', 'drawing');
-    can.width = window.innerWidth - 10;
-    can.height = window.innerHeight - 110;
-    document.getElementById("container").appendChild(can);
-    drawer = new Drawer(can);
+    if (!document.getElementById('drawing')) {
+        var can = document.createElement('canvas');
+        can.setAttribute('id', 'drawing');
+        can.width = window.innerWidth - 10;
+        can.height = window.innerHeight - 110;
+        document.getElementById("container").appendChild(can);
+        drawer = new Drawer(can);
+    }
 };
--- SoftBody/BUILD_TS/controller.ts
+++ SoftBody/BUILD_TS/controller.ts
@@ -1,10 +1,12 @@
 let drawer: Drawer | null = null;
 
 let create = () => {
-    let can = document.createElement('canvas') as HTMLCanvasElement;
-    can.setAttribute('id', 'drawing');
-    can.width = window.innerWidth - 10;
-    can.height = window.innerHeight - 110;
-    document.getElementById("container").appendChild(can);
-    drawer = new Drawer(can);
+    if (!document.getElementById('drawing')) {
+        let can = document.createElement('canvas') as HTMLCanvasElement;
+        can.setAttribute('id', 'drawing');
+        can.width = window.innerWidth - 10;
+        can.height = window.innerHeight - 110;
+        document.getElementById("container").appendChild(can);
+        drawer = new Drawer(can);
+    }
 }
